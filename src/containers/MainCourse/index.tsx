ccde944eabdc3713327693538104241f7d1987b2
instance <ilyaxinevich@gmail.com>
2022-05-05 18:02:29 +0300

    prev

--- src/containers/MainCourse/index.tsx
+++ src/containers/MainCourse/index.tsx
@@ -191,7 +191,7 @@ const MainCourse: React.FC<TMainCourse> = ({ id }) => {
                 <button
                   id="dropdownDefault"
                   data-dropdown-toggle={`dropdowngroup`}
-                  className="w-28 text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-4 py-2.5 text-center inline-flex items-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
+                  className="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-4 py-2.5 text-center inline-flex items-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
                   type="button"
                 >
                   Группы
